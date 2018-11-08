332db391eff8d76b48fd8ff162840f3be1145cb3
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 16:55:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,21 +8,14 @@ client.on('ready', () => {
     console.log('---------------')
   });
 
-client.on("message", message => {
-    var prefix = "$";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
-        var lmt = message.content.split(' ')[1]
-    var msg;
-          msg = parseInt();
-        
-       
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.send('`done`').then(m => m.delete(1000));
-    }
+    client.on('message', message => {
+        if (message.content.startWith(prefix + "deleterole")){
+    if(!message.channel.guild) return;
+     message.guild.roles.forEach( roles => {
+         
+         roles.delete()
+     })
+}
 });
 
 client.login(process.env.BOT_TOKEN);
