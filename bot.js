d489b73dfe04daec747d7044dfa6cfcba7dee97a
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 16:20:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,17 +16,18 @@ client.on('message', msg => {
   command = command.slice(prefix.length);
   let args = msg.content.split(" ").slice(1);
 
-    if(command === "purge") {
-        const emoji = client.emojis.find("name", ":wastebasket:")
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
     let textxt = args.slice(0).join("");
     if(msg.member.hasPermission("MANAGE_MESSAGES")) {
     if (textxt == "") {
-    msg.channel.send("ضع عددا من الرسائل التي تريد مسحها");
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(3000));
 } else {
     msg.delete().then
     msg.delete().then
     msg.channel.bulkDelete(textxt);
-    msg.channel.send(`${emoji} Deleted ` + "`" + textxt + "` messages");
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
         }    
     }
 }
